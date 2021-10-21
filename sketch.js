6751d0d77837c98fcd3e68624d3755f792b0294f
Zane Golas <zgolas@icloud.com>
2021-10-21 00:03:32 -0700

    V 0.50 Added video and dynamite clearing functions

--- sketch.js
+++ sketch.js
@@ -1,13 +1,18 @@
 let button0, button1, button2, button3;
 let brushSelect, typeIndex, myFont, textBank, typeMax;
+let vid, img;
 
 function preload() {
   myFont = loadFont('assets/Inconsolata-Regular.ttf');
   textBank = loadStrings('assets/text_bank.txt');
+  vid = createVideo('assets/video.mp4');
+  vid.hide();
+  img = loadImage('assets/cursor-tnt-anim.gif');
+
 }
 
 function setup() {
-  createCanvas(1000, 800, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   background(0);
   rectMode(CENTER);
 
@@ -42,23 +47,33 @@ function draw() {
   if (brushSelect === 1){
     typeText();
   }
+  if (brushSelect === 2){
+    paintVideo();
+  }
+  if (brushSelect === 3){
+    blowItUp();
+  }
 
 }
 
 function brush0(){
   brushSelect = 0;
+  vid.pause();
 }
 
 function brush1(){
   brushSelect = 1;
+  vid.pause();
 }
 
 function brush2(){
   brushSelect = 2;
+  vid.loop();
 }
 
 function brush3(){
   brushSelect = 3;
+  vid.pause();
 }
 
 function paintCube(){
@@ -89,6 +104,30 @@ function typeText(){
       typeIndex = 0;
     }
   }
+
+}
+
+function paintVideo() {
+  if (true) {
+    texture(vid);
+    translate(mouseX - width / 2, mouseY - height / 2, 0);
+    push();
+    rotateZ(frameCount * 0.01);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
+    box(width / 10);
+    pop();
+  }
+}
+
+function blowItUp (){
+  translate(mouseX - width / 2, mouseY - height / 2, 0);
+  image(img, 0, 0);
+  if (!mouseIsPressed){
+    background(0);
+    brush1();
+  }
+
 
 }
 
